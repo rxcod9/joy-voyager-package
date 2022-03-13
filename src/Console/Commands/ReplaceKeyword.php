2b3dd0ccc3ddb49c52c9eaebd269fe8d5878776d
Ramakant <gangwar.ramakant@gmail.com>
2022-03-13 19:01:47 +0530

    A new start

---
+++ src/Console/Commands/ReplaceKeyword.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Joy\VoyagerReplaceKeyword\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Models\DataType;
+
+class ReplaceKeyword extends Command
+{
+    protected $name = 'joy-replace-keyword';
+
+    protected $description = 'Joy Voyager ReplaceKeyworder';
+
+    public function handle()
+    {
+        $this->output->title('Starting replace-keyword');
+
+        // Your magic here
+
+        $this->output->success('ReplaceKeyword successful');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['arguement1', InputArgument::REQUIRED, 'The argument1 description'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            [
+                'option1',
+                'o',
+                InputOption::VALUE_OPTIONAL,
+                'The option1 description',
+                config('joy-voyager-replace-keyword.option1')
+            ],
+        ];
+    }
+}
